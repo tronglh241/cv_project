c985b971d9fe45838776c9363f381feda9c5349f
Trong Le Huu <tronglh.241@gmail.com>
2020-03-15 20:58:07 +0700

    Update DAO to dict conversion

--- abstract/dao.py
+++ abstract/dao.py
@@ -1,20 +1,21 @@
 class DAO(object):
-    def asdict(self):
-        def _asdict(obj):
-            if isinstance(obj, dict):
-                return {key: _asdict(value) for key, value in obj.items()}
-            elif isinstance(obj, list):
-                return [_asdict(element) for element in obj]
-            elif isinstance(obj, tuple):
-                return tuple([_asdict(element) for element in obj])
-            elif isinstance(obj, (int, float, str, bool)):
-                return obj
-            elif isinstance(obj, DAO):
-                return obj.asdict()
-            else:
-                raise ValueError('Unsupported type {}.'.format(type(obj)))
+    @staticmethod
+    def _asdict(obj):
+        if isinstance(obj, dict):
+            return {key: DAO._asdict(value) for key, value in obj.items()}
+        elif isinstance(obj, list):
+            return [DAO._asdict(element) for element in obj]
+        elif isinstance(obj, tuple):
+            return tuple([DAO._asdict(element) for element in obj])
+        elif isinstance(obj, (int, float, str, bool)):
+            return obj
+        elif isinstance(obj, DAO):
+            return obj.asdict()
+        else:
+            raise ValueError('Unsupported type {}.'.format(type(obj)))
 
-        return {key: _asdict(value) for key, value in self.__dict__.items()}
+    def asdict(self):
+        return {key: DAO._asdict(value) for key, value in self.__dict__.items()}
 
     def __repr__(self):
         _repr = '{}('.format(self.__class__.__name__)
