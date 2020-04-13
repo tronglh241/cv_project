9a13496ae25cf474dc6b93b52462b9b360014f0b
Trong Le Huu <tronglh.241@gmail.com>
2020-04-13 15:47:14 +0700

    Make default value for Stage's config_path

--- abstract/stage.py
+++ abstract/stage.py
@@ -1,10 +1,14 @@
 import utils
+import inspect
 
 from abstract.processor import Processor
 
 
 class Stage(object):
-    def __init__(self, mode, config_path, *args, **kwargs):
+    def __init__(self, mode=None, config_path=None, *args, **kwargs):
+        if config_path is None:
+            config_path = utils.Path(inspect.getfile(self.__class__)).with_name('config.yaml')
+        
         if mode is None:
             self.processor = Processor()
         elif config_path.exists():
--- modules/number_detection/number_detection.py
+++ modules/number_detection/number_detection.py
@@ -6,9 +6,6 @@ from abstract.dao import DAO
 
 
 class NumberDetection(Stage):
-    def __init__(self, mode=None, config_path=utils.Path(__file__).with_name('config.yaml')):
-        super(NumberDetection, self).__init__(mode, config_path)
-
     def preprocess(self, image_path):
         info = Info(image_path)
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
